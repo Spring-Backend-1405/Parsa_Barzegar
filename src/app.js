18be87aa6c779c6ac053a8fb1b5c882af2317146
Parsa Barzegar <parsabarzegar.88@gmail.com>
2026-06-30 21:21:02 +0330

    feat(auth): implement model of user & make auth structure

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 import express from "express";
+import authRouter from "./module/auth/auth.router.js";
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/" , (req,res) => {
-    res.json({
-        message:"this is just for test"
-    })
-})
+app.use("/auth", authRouter);
+
+app.use("/", (req, res) => {
+  res.json({
+    message: "this is just for test",
+  });
+});
 
 export default app;
